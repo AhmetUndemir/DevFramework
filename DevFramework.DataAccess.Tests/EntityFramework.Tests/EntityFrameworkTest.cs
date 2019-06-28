3b0dc84b5008c6d7b85aa7c767f5f80047a01333
Ahmet <ahmetundemir@aof.anadolu.edu.tr>
2019-06-28 22:49:37 +0300

    Category için arayüz framework implementasyonları yapıldı.

--- DevFramework.DataAccess.Tests/EntityFramework.Tests/EntityFrameworkTest.cs
+++ DevFramework.DataAccess.Tests/EntityFramework.Tests/EntityFrameworkTest.cs
@@ -15,5 +15,14 @@ namespace DevFramework.DataAccess.Tests.EntityFramework.Tests
 
             Assert.AreEqual(78, result.Count);
         }
+
+        [TestMethod]
+        public void Get_all_with_parameter_returns_filtered_products()
+        {
+            EfProductDal productDal = new EfProductDal();
+            var result = productDal.GetList(p=>p.ProductName.Contains("ab"));
+
+            Assert.AreEqual(4, result.Count);
+        }
     }
 }
