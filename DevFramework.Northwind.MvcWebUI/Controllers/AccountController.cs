8d2f903f389cae7b54944df08ad5d1b8fb21ded0
Ahmet <ahmetundemir@aof.anadolu.edu.tr>
2019-07-20 15:27:59 +0300

    User'ın arayüz ve iş katmanı implementasyonu

--- DevFramework.Northwind.MvcWebUI/Controllers/AccountController.cs
+++ DevFramework.Northwind.MvcWebUI/Controllers/AccountController.cs
@@ -4,18 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DevFramework.Core.CrossCuttingConcerns.Security.Web;
+using DevFramework.Northwind.Business.Abstract;
 
 namespace DevFramework.Northwind.MvcWebUI.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Account
-        public string Login()
+        private IUserService _userService;
+
+        public AccountController(IUserService userService)
         {
-            AuthenticationHelper.CreateAuthCookie(
-                new Guid(),"ahmetundemir","ahmetundemir@gmail.com",DateTime.Now.AddDays(15),
-                new []{"Admin"},false,"Ahmet","Ündemir");
-            return "User is authenticated!";
+            _userService = userService;
+        }
+
+        // GET: Account
+        public string Login(string userName,string password)
+        {
+            var user = _userService.GetByUserNameAndPassword(userName,password);
+            if (user != null)
+            {
+                AuthenticationHelper.CreateAuthCookie(
+                new Guid(), user.UserName, user.Email, DateTime.Now.AddDays(15),
+                new[] { "Admin" }, false, user.FirstName, user.LastName);
+                return "User is authenticated!";
+            }
+
+            return "User is Not Authenticated!";
+
+
         }
     }
 }
