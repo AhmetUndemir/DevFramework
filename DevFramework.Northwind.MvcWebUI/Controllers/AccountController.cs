635d0bbf6089890e5c0658e8fd10dd98f80baa56
Ahmet <ahmetundemir@aof.anadolu.edu.tr>
2019-07-20 14:48:58 +0300

    SecurityUtlities testi

---
+++ DevFramework.Northwind.MvcWebUI/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DevFramework.Core.CrossCuttingConcerns.Security.Web;
+
+namespace DevFramework.Northwind.MvcWebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public string Login()
+        {
+            AuthenticationHelper.CreateAuthCookie(
+                new Guid(),"ahmetundemir","ahmetundemir@gmail.com",DateTime.Now.AddDays(15),
+                new []{"Admin"},false,"Ahmet","Ündemir");
+            return "User is authenticated!";
+        }
+    }
+}
